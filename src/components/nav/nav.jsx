74ae5e64b3569a226f2ced2ca2a8751d0502bcb9
Joel-HackMaster <u21216300@utp.edu.pe>
2024-10-09 19:53:41 -0500

    third commit

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -27,6 +27,7 @@ export default function nav() {
 
     const handleNavigation = (event, path) => {
         event.preventDefault();
+        setMobileMenuOpen(false);
         navigate(path);
     };
 
